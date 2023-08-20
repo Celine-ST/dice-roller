8f91be33ad20bc4c8d44831b164cd80f915ca88e
Celine-ST <celine.quek10@rafflesgirlssch.edu.sg>
2023-08-20 12:58:22 +0800

    Initial Commit

---
+++ dice-roller/dice_rollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  dice_rollerApp.swift
+//  dice-roller
+//
+//  Created by Celine Quek on 20/8/23.
+//
+
+import SwiftUI
+
+@main
+struct dice_rollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
